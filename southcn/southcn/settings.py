86a49982554c653501c9f12037a9a3399a0420cb
xiaoqi17 <ftkhg@163.com>
2017-09-08 22:40:12 +0800

    Update settings.py

--- southcn/southcn/settings.py
+++ southcn/southcn/settings.py
@@ -45,7 +45,6 @@ DEFAULT_REQUEST_HEADERS = {
     'Accept-Language':'zh-CN,zh;q=0.8',
     'Cache-Control':'max-age=0',
     'Connection':'keep-alive',
-    # 'Cookie':'PHPSESSID=7j3ceak5g45jqh1ikjiobj7t27; wdcid=4926115a4e8efe5b; wdses=65021e8640b15130; _pk_id.4.05e6=5fec4ae8d869f6f6.1504856188.1.1504856208.1504856188.; cn_1260591378_dplus=%7B%22distinct_id%22%3A%20%2215e606b25a8215-0349d0bbfaf5a1-63151074-1fa400-15e606b25a9a6c%22%2C%22sp%22%3A%20%7B%22user_id%22%3A%20%225fec4ae8d869f6f6%22%2C%22%24_sessionid%22%3A%200%2C%22%24_sessionTime%22%3A%201504856209%2C%22%24dp%22%3A%200%2C%22%24_sessionPVTime%22%3A%201504856209%7D%2C%22initial_view_time%22%3A%20%221504851177%22%2C%22initial_referrer%22%3A%20%22http%3A%2F%2Fedu.southcn.com%2F%22%2C%22initial_referrer_domain%22%3A%20%22edu.southcn.com%22%7D; UM_distinctid=15e606b25a8215-0349d0bbfaf5a1-63151074-1fa400-15e606b25a9a6c; Hm_lvt_fcda14e8d9fc166be9cf6caef393ad0e=1504851237; Hm_lpvt_fcda14e8d9fc166be9cf6caef393ad0e=1504858133; __asc=ad519a5e15e6062b12c26d24d67; __auc=343eb0c215e601f9ce368176658; wdlast=1504858133',
     'Host':'epaper.southcn.com',
     'Upgrade-Insecure-Requests':'1',
     'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.86 Safari/537.36'
@@ -73,7 +72,6 @@ DEFAULT_REQUEST_HEADERS = {
 # See http://scrapy.readthedocs.org/en/latest/topics/item-pipeline.html
 ITEM_PIPELINES = {
    'southcn.pipelines.MongoPipeline': 300,
-   'southcn.pipelines.SouthcnPipeline': 301,
 }
 
 # Enable and configure the AutoThrottle extension (disabled by default)
@@ -96,3 +94,4 @@ ITEM_PIPELINES = {
 #HTTPCACHE_DIR = 'httpcache'
 #HTTPCACHE_IGNORE_HTTP_CODES = []
 #HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
+DUPEFILTER_CLASS='scrapy.dupefilters.RFPDupeFilter'
